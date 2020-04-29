2665ebfda321117ee5e510b515f05619a7039d9d
MrLoh <flojoto@hotmail.com>
2020-04-29 14:51:54 -0500

    upgrade android support import

--- android/src/main/java/com/selligent/RNSelligent.java
+++ android/src/main/java/com/selligent/RNSelligent.java
@@ -6,7 +6,7 @@ import android.app.Application;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.res.Resources;
-import android.support.v4.content.LocalBroadcastManager;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 import android.util.Log;
 
 import com.facebook.react.bridge.ActivityEventListener;
